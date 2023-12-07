9597aad8c97fa413d7a4ed26df8ad26f688a157e
IsaiZ734 <isaimola@gmail.com>
2023-12-07 20:47:08 +0100

    test-and-set finished

--- headers/test-and-set.h
+++ headers/test-and-set.h
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
 
 typedef struct mutex{
     volatile int *lock;
 } my_mutex;
 
-int init(my_mutex *mutex);
+typedef struct threads_args {
+    my_mutex *mutex;
+    int n_cycles;
+} my_threads_args;
+
+
+int my_mutex_init(my_mutex *mutex);
+
+int my_mutex_destroy(my_mutex *mutex);
 
 int my_mutex_lock(my_mutex *mutex);
 
